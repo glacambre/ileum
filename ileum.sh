18a53e6f11b45e8c1160504ff1e15dd14067b629
glacambre <code@lacamb.re>
2021-10-08 07:39:48 +0200

    Prevent plugin_dir collision, check NVIM_LISTEN_ADDRESS

--- ileum.sh
+++ ileum.sh
@@ -1,11 +1,13 @@
 
-plugin_dir="$(dirname "$0")"
+ILEUM_PLUGIN_DIR="$(dirname "$0")"
 command_not_found_handler() {
   if [[ ":" == "${0:0:1}" ]]; then
-    if which nvim >/dev/null 2>/dev/null ; then
+    if [ -n "$NVIM_LISTEN_ADDRESS" ]; then
       args="${(qqq)@}"
       pwd="${(qqq)PWD}"
-      nvim -u NONE -i NONE --headless --cmd "source $plugin_dir/ileum.vim" --cmd ":call Ileum($pwd,'$NVIM_LISTEN_ADDRESS',$args)"
+      nvim -u NONE -i NONE --headless --cmd "source $ILEUM_PLUGIN_DIR/ileum.vim" --cmd ":call Ileum($pwd,'$NVIM_LISTEN_ADDRESS',$args)"
+    else
+      echo "ileum: NVIM_LISTEN_ADDRESS undefined"
     fi
   else
     echo "commant not found: $@"
