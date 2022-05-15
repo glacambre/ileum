90de39f8b8bf7f301ff7dd30b5445a4cbd1ad1f7
glacambre <code@lacamb.re>
2022-05-15 15:12:48 +0200

    Check both $NVIM_LISTEN_ADDRESS and $NVIM
    Closes #1

--- ileum.sh
+++ ileum.sh
@@ -2,14 +2,18 @@
 ILEUM_PLUGIN_DIR="$(dirname "$0")"
 command_not_found_handler() {
   if [[ ":" == "${0:0:1}" ]]; then
-    if [ -n "$NVIM_LISTEN_ADDRESS" ]; then
+    ADDR="$NVIM_LISTEN_ADDRESS"
+    if [ -z "$ADDR" ]; then
+      ADDR="$NVIM"
+    fi
+    if [ -n "$ADDR" ]; then
       args="${(qqq)@}"
       pwd="${(qqq)PWD}"
-      nvim -u NONE -i NONE --headless --cmd "source $ILEUM_PLUGIN_DIR/ileum.vim" --cmd ":call Ileum($pwd,'$NVIM_LISTEN_ADDRESS',$args)"
+      nvim -u NONE -i NONE --headless --cmd "source $ILEUM_PLUGIN_DIR/ileum.vim" --cmd ":call Ileum($pwd,'$ADDR',$args)"
     else
-      echo "ileum: NVIM_LISTEN_ADDRESS undefined"
+      echo "ileum: NVIM_LISTEN_ADDRESS and NVIM undefined"
     fi
   else
-    echo "commant not found: $@"
+    echo "command not found: $@"
   fi
 }
